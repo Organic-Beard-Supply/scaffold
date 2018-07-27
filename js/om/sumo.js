71a2fe1892db30b58c1ae25c995a87e2f96a2ab7
yaboi <tyler@tylergoelz.com>
2018-07-27 17:38:20 -0400

    Update footer, create dark logo, update favicon/icon. Add sumo click trigger listbuilders for joining om tribe and signing up early for gatherings. Add terms and privacy modals w/ content ported over from Shopify. Update all links to make sure they either point back to the landing page (scrollto) or a proper destination.

---
+++ js/om/sumo.js
@@ -0,0 +1,1 @@
+(function(s,u,m,o,j,v){j=u.createElement(m);v=u.getElementsByTagName(m)[0];j.async=1;j.src=o;j.dataset.sumoSiteId='6c86e8a6b50ee3198b375a7ae3c243658f590f24f5852bda74461478fe684159';v.parentNode.insertBefore(j,v)})(window,document,'script','//load.sumo.com/');
